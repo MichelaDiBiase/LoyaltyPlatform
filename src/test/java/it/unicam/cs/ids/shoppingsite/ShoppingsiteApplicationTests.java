2ee03f0c88cc5b0d23910a5d8293c5c9e3840314
MichelaDiBiase <126597282+MichelaDiBiase@users.noreply.github.com>
2023-05-20 12:11:33 +0200

    Creating project

---
+++ src/test/java/it/unicam/cs/ids/shoppingsite/ShoppingsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unicam.cs.ids.shoppingsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
