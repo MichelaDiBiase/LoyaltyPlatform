5dc42733193a87a25bc221fd57f3cb1259ef3633
MichelaDiBiase <michela.dibiase@studenti.unicam.it>
2023-05-25 16:30:11 +0200

    Creating classes

--- src/main/java/it/unicam/cs/ids/shoppingsite/shoppingservices/Product.java
+++ src/main/java/it/unicam/cs/ids/shoppingsite/shoppingservices/Product.java
@@ -24,4 +24,59 @@ public class Product {
 
 	}
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public Agency getAgency() {
+		return agency;
+	}
+
+	public void setAgency(Agency agency) {
+		this.agency = agency;
+	}
+
+	public int getPoints() {
+		return points;
+	}
+
+	public void setPoints(int points) {
+		this.points = points;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public List<Review> getListReviews() {
+		return listReviews;
+	}
+
+	public void setListReviews(List<Review> listReviews) {
+		this.listReviews = listReviews;
+	}
 }
