83039df6e418b90febfd1d08ca346dbcb6bc987d
MichelaDiBiase <michela.dibiase@studenti.unicam.it>
2023-06-07 13:05:14 +0200

    Edit class ProductConnection Edit application properties

--- src/main/java/it/unicam/cs/ids/shoppingsite/connection/ProductConnection.java
+++ src/main/java/it/unicam/cs/ids/shoppingsite/connection/ProductConnection.java
@@ -1,15 +1,74 @@
 package it.unicam.cs.ids.shoppingsite.connection;
 
-import java.sql.Connection;
+import it.unicam.cs.ids.shoppingsite.shoppingservices.Review;
+import it.unicam.cs.ids.shoppingsite.users.Agency;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
+@Entity
 public class ProductConnection {
-    Connection connection;
+    @Id
+    private Integer id;
+    private String name;
+    private Double price;
+    private Agency agency;
+    private Integer pints;
+    private String decription;
+    private Review review;
 
-    public ProductConnection() {
-        try {
-            connection = DBConnection.getConnection();
-        } catch (Exception e) {
-            System.out.println(e);
-        }
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Agency getAgency() {
+        return agency;
+    }
+
+    public void setAgency(Agency agency) {
+        this.agency = agency;
+    }
+
+    public Integer getPints() {
+        return pints;
+    }
+
+    public void setPints(Integer pints) {
+        this.pints = pints;
+    }
+
+    public String getDecription() {
+        return decription;
+    }
+
+    public void setDecription(String decription) {
+        this.decription = decription;
+    }
+
+    public Review getReview() {
+        return review;
+    }
+
+    public void setReview(Review review) {
+        this.review = review;
     }
 }
