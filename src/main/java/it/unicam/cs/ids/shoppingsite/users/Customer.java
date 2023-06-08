c19b59b4faeb6ddf6cf9fbe94b0809b882d275d7
Andre10001 <andrea.mazzetti@studenti.unicam.it>
2023-06-08 16:46:25 +0200

    Customer class and Agency class was modified IUser interface was created

--- src/main/java/it/unicam/cs/ids/shoppingsite/users/Customer.java
+++ src/main/java/it/unicam/cs/ids/shoppingsite/users/Customer.java
@@ -1,59 +1,45 @@
 package it.unicam.cs.ids.shoppingsite.users;
 
-import it.unicam.cs.ids.shoppingsite.mailbox.Mailbox;
-import it.unicam.cs.ids.shoppingsite.models.ICustomer;
-import it.unicam.cs.ids.shoppingsite.models.RolesType;
-import it.unicam.cs.ids.shoppingsite.premiumprogram.FidelityCard;
-import it.unicam.cs.ids.shoppingsite.shoppingservices.Catalog;
-import it.unicam.cs.ids.shoppingsite.shoppingservices.Product;
-import it.unicam.cs.ids.shoppingsite.shoppingservices.ShoppingCart;
+import it.unicam.cs.ids.shoppingsite.models.IUser;
+import jakarta.persistence.*;
 
-import javax.management.relation.Role;
-
-public class Customer implements ICustomer {
-
+@Entity
+@Table(name = "customers")
+public class Customer implements IUser {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	@Column(nullable = false)
 	private String name;
+	@Column(nullable = false)
 	private String surname;
+	@Id
+	@Column(nullable = false)
 	private String email;
-	private String gender;
-	private ShoppingCart shoppingCart;
-	private Mailbox mailBox;
-	private RolesType role;
+	@Column(nullable = false)
+	private String password;
+	@Column(nullable = false)
+	private Boolean gender;
 
-	public Customer(String name, String surname, String email, String gender) {
+	public Customer(String name, String surname, String email, Boolean gender) {
 		this.name = name;
 		this.surname = surname;
 		this.email = email;
 		this.gender = gender;
-		this.shoppingCart = new ShoppingCart();
-		this.mailBox = new Mailbox();
-		this.role = RolesType.basic;
 	}
 
-	public void editProfile(String name, String surname, String email, String gender) {
+	/*
+	public void editProfile(String name, String surname, String email, Boolean gender) {
 		setName(name);
 		setSurname(surname);
 		setEmail(email);
 		setGender(gender);
 	}
+	 */
 
-	public void buyProduct() {
-		// TODO - implement Customer.buyProduct
-		throw new UnsupportedOperationException();
+	public Integer getId() {
+		return id;
 	}
-
-	public void addToShoppingCart(Product product, int quantity) {
-		shoppingCart.addProductQuantity(product, quantity);
-	}
-
-	public void removeFromShoppingCart(Product product, int quantity) {
-		shoppingCart.removeProductQuantity(product, quantity);
-	}
-
-	public void upgradeRole(RolesType role) {
-		this.role = role;
-	}
-
 	public String getName() {
 		return name;
 	}
@@ -62,22 +48,22 @@ public class Customer implements ICustomer {
 		return surname;
 	}
 
+	@Override
 	public String getEmail() {
 		return email;
 	}
 
-	public String getGender() {
+	@Override
+	public String getPassword() {
+		return password;
+	}
+
+	public Boolean getGender() {
 		return gender;
 	}
-
-	public Mailbox getMailBox() {
-		return mailBox;
+	public void setId(Integer id) {
+		this.id = id;
 	}
-
-	public RolesType getRole() {
-		return role;
-	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
@@ -90,8 +76,7 @@ public class Customer implements ICustomer {
 		this.email = email;
 	}
 
-	public void setGender(String gender) {
+	public void setGender(Boolean gender) {
 		this.gender = gender;
 	}
-
 }
