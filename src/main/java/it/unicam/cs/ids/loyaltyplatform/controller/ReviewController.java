0160dd4e270e11f537b95f14b07ce38a897e285a
MichelaDiBiase <michela.dibiase@studenti.unicam.it>
2023-06-21 11:43:02 +0200

    add constructor in ReviewController ad ReviewService

--- src/main/java/it/unicam/cs/ids/loyaltyplatform/controller/ReviewController.java
+++ src/main/java/it/unicam/cs/ids/loyaltyplatform/controller/ReviewController.java
@@ -1,4 +1,16 @@
 package it.unicam.cs.ids.loyaltyplatform.controller;
 
+import it.unicam.cs.ids.loyaltyplatform.service.ReviewService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
 public class ReviewController {
+
+    private final ReviewService reviewService;
+
+    public ReviewController(ReviewService reviewService) {
+        this.reviewService = reviewService;
+    }
 }
--- src/main/java/it/unicam/cs/ids/loyaltyplatform/service/ReviewService.java
+++ src/main/java/it/unicam/cs/ids/loyaltyplatform/service/ReviewService.java
@@ -1,4 +1,13 @@
 package it.unicam.cs.ids.loyaltyplatform.service;
 
+import it.unicam.cs.ids.loyaltyplatform.repository.ReviewRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ReviewService {
+    private final ReviewRepository reviewRepository;
+
+    public ReviewService(ReviewRepository reviewRepository) {
+        this.reviewRepository = reviewRepository;
+    }
 }
