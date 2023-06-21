1bbdbe735b1fbe2ca958f74f0c8abd217af25b4a
MichelaDiBiase <michela.dibiase@studenti.unicam.it>
2023-06-21 11:41:46 +0200

    ReviewRepository interface extends JapRepository

--- src/main/java/it/unicam/cs/ids/loyaltyplatform/repository/ReviewRepository.java
+++ src/main/java/it/unicam/cs/ids/loyaltyplatform/repository/ReviewRepository.java
@@ -1,4 +1,8 @@
 package it.unicam.cs.ids.loyaltyplatform.repository;
 
-public interface ReviewRepository {
+import it.unicam.cs.ids.loyaltyplatform.entity.platformservices.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewRepository extends JpaRepository<Product, Integer> {
+
 }
