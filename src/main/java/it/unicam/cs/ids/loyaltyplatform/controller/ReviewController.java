7f622a2a5c110f6b2754e8ecb842c78a128093fe
MichelaDiBiase <michela.dibiase@studenti.unicam.it>
2023-06-22 17:07:45 +0200

    edit ReviewController

--- src/main/java/it/unicam/cs/ids/loyaltyplatform/controller/ReviewController.java
+++ src/main/java/it/unicam/cs/ids/loyaltyplatform/controller/ReviewController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping
+@RequestMapping("/review")
 public class ReviewController {
 
     private final ReviewService reviewService;
