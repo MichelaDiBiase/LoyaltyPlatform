4c43a025e0ce3af730b7cf7ad1aceee122d84762
MichelaDiBiase <michela.dibiase@studenti.unicam.it>
2023-06-22 17:08:35 +0200

    edit MailRepository

--- src/main/java/it/unicam/cs/ids/loyaltyplatform/repository/MailRepository.java
+++ src/main/java/it/unicam/cs/ids/loyaltyplatform/repository/MailRepository.java
@@ -2,6 +2,8 @@ package it.unicam.cs.ids.loyaltyplatform.repository;
 
 import it.unicam.cs.ids.loyaltyplatform.entity.platformservices.Mail;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MailRepository extends JpaRepository<Mail, Integer> {
 }
