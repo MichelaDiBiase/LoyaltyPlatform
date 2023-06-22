0c8904dd41ced7139ef05b50b7bccd79b23359c2
MichelaDiBiase <michela.dibiase@studenti.unicam.it>
2023-06-22 15:58:18 +0200

    edit MailService

--- src/main/java/it/unicam/cs/ids/loyaltyplatform/service/MailService.java
+++ src/main/java/it/unicam/cs/ids/loyaltyplatform/service/MailService.java
@@ -1,7 +1,10 @@
 package it.unicam.cs.ids.loyaltyplatform.service;
 
+import it.unicam.cs.ids.loyaltyplatform.entity.platformservices.Mail;
 import it.unicam.cs.ids.loyaltyplatform.repository.MailRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MailService {
@@ -11,4 +14,23 @@ public class MailService {
 
         this.mailRepository = mailRepository;
     }
+    public void addMail(Mail mail) {
+
+         this.mailRepository.save(mail);
+    }
+    public void deleteMail(Mail mail) {
+
+        this.mailRepository.delete(mail);
+    }
+    public List<Mail> getMailByCustomerId(Integer idCustomer){
+        return this.mailRepository.findAll().parallelStream().filter(x -> x.getIdCostumer()==(idCustomer)).toList();
+    }
+    public List<Mail> getMailByAgencyId(Integer idAgency){
+        return this.mailRepository.findAll().parallelStream().filter(x -> x.getIdAgency()==(idAgency)).toList();
+    }
+
+    public List<Mail> getMailByCustomerIdAndAgencyId(Integer idCustomer, Integer idAgency) {
+        return this.mailRepository.findAll().parallelStream().filter(x -> x.getIdCostumer() == idCustomer && x.getIdAgency() == idAgency).toList();
+    }
+
 }
