388c69c9306539643b4d491f4baf62998f83f9ba
MichelaDiBiase <michela.dibiase@studenti.unicam.it>
2023-06-22 11:03:07 +0200

    edit MailService ProductRepository

--- src/main/java/it/unicam/cs/ids/loyaltyplatform/repository/ProductRepository.java
+++ src/main/java/it/unicam/cs/ids/loyaltyplatform/repository/ProductRepository.java
@@ -1,6 +1,7 @@
 package it.unicam.cs.ids.loyaltyplatform.repository;
 
 import it.unicam.cs.ids.loyaltyplatform.entity.platformservices.Product;
+import it.unicam.cs.ids.loyaltyplatform.entity.platformservices.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
--- src/main/java/it/unicam/cs/ids/loyaltyplatform/service/MailService.java
+++ src/main/java/it/unicam/cs/ids/loyaltyplatform/service/MailService.java
@@ -1,4 +1,14 @@
 package it.unicam.cs.ids.loyaltyplatform.service;
 
+import it.unicam.cs.ids.loyaltyplatform.repository.MailRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class MailService {
+    private final MailRepository mailRepository;
+
+    public MailService(MailRepository mailRepository) {
+
+        this.mailRepository = mailRepository;
+    }
 }
