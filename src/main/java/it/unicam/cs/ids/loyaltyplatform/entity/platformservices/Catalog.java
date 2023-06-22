7a2d8799726ca1430b1ae4c353e1b6338d6cea28
MichelaDiBiase <michela.dibiase@studenti.unicam.it>
2023-06-22 17:24:09 +0200

    Edit Catalog

--- src/main/java/it/unicam/cs/ids/loyaltyplatform/entity/platformservices/Catalog.java
+++ src/main/java/it/unicam/cs/ids/loyaltyplatform/entity/platformservices/Catalog.java
@@ -1,8 +1,57 @@
 package it.unicam.cs.ids.loyaltyplatform.entity.platformservices;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
-
-
+@Entity
+@Table(name = "catalog")
 public class Catalog {
+    @Id
+    @Column(nullable = false,unique = true)
+    private Integer id;
+    @Column(nullable = false)
+    private Integer idProduct;
+    @Column(nullable = false)
+    private Integer points;
+    @Column(nullable = false)
+    private Integer quantity;
 
 
+    public Catalog(int idProduct,Integer points,Integer quantity){
+        this.idProduct=idProduct;
+        this.points=points;
+        this.quantity=quantity;
+
+    }
+
+
+    public Catalog() {
+
+    }
+
+
+    public Integer getIdProduct() {
+        return idProduct;
+    }
+
+    public void setIdProduct(Integer idProduct) {
+        this.idProduct = idProduct;
+    }
+
+    public Integer getPoints() {
+        return points;
+    }
+
+    public void setPoints(Integer points) {
+        this.points = points;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
 }
