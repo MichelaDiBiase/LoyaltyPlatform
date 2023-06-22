5283dfe4c44f9f0237f4b899a549d60fc051d1e2
MichelaDiBiase <michela.dibiase@studenti.unicam.it>
2023-06-22 16:40:13 +0200

    created Catalog

--- src/main/java/it/unicam/cs/ids/loyaltyplatform/entity/platformservices/Catalog.java
+++ src/main/java/it/unicam/cs/ids/loyaltyplatform/entity/platformservices/Catalog.java
@@ -1,57 +1,8 @@
 package it.unicam.cs.ids.loyaltyplatform.entity.platformservices;
 
-import java.util.List;
+
 
 public class Catalog {
 
-    private List<Product> listProduct;
-    private List<Review> listReviews;
-
-    public Catalog(List<Product> listProduct,List<Review> listReviews){
-        this.listProduct=listProduct;
-        this.listReviews=listReviews;
-    }
-
-    /**
-     * @param product
-     */
-    public void addProductQuantity(Product product, int quantity) {
-        listProduct.add(product);
-
-    }
-
-    /**
-     * @param product
-     */
-    public void removeProductQuantity(Product product, int quantity) {
-        listProduct.remove(product);
-    }
-
-    /**
-     * @param review
-     */
-    public void addReviews(Review review) {//TODO add ptoduct?
-        listReviews.add(review);
-    }
-
-    /**
-     * @param nameProduct
-     */
-    public Product searchProduct(String nameProduct) {
-
-        for (Product product : listProduct){
-            if(nameProduct.equals(product.getName())){
-                return product;
-            }
-
-        }
-        return null;
-    }
-
-    public void printProductsList() {
-        for (Product product : listProduct) {
-            System.out.println(product);
-        }
-    }
 
 }
