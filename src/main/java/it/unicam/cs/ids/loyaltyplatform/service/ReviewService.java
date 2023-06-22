5ba1af62b1eb488eff7bcdd5f75ea2618e447f16
MichelaDiBiase <michela.dibiase@studenti.unicam.it>
2023-06-22 15:59:06 +0200

    edit ReviewService

--- src/main/java/it/unicam/cs/ids/loyaltyplatform/service/ReviewService.java
+++ src/main/java/it/unicam/cs/ids/loyaltyplatform/service/ReviewService.java
@@ -1,7 +1,11 @@
 package it.unicam.cs.ids.loyaltyplatform.service;
 
+
+import it.unicam.cs.ids.loyaltyplatform.entity.platformservices.Review;
 import it.unicam.cs.ids.loyaltyplatform.repository.ReviewRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ReviewService {
@@ -10,4 +14,33 @@ public class ReviewService {
     public ReviewService(ReviewRepository reviewRepository) {
         this.reviewRepository = reviewRepository;
     }
+
+    public void addReview(Review review) {
+
+        this.reviewRepository.save(review);
+    }
+
+    public void deleteReview(Review review) {
+
+        this.reviewRepository.delete(review);
+    }
+
+    public void updateReview(Review review) {
+        this.reviewRepository.saveAndFlush(review);
+    }
+
+    public List<Review> getReviewByCustomerId(Integer idCustomer) {
+        return this.reviewRepository.findAll().parallelStream().filter(x -> x.getIdCostumer() == (idCustomer)).toList();
+
+    }
+
+    public List<Review> getReviewByAgencyId(Integer idAgency) {
+        return this.reviewRepository.findAll().parallelStream().filter(x -> x.getIdAgency() == (idAgency)).toList();
+    }
+
+    public List<Review> getReviewByCustomerIdAndAgencyId(Integer idCustomer, Integer idAgency) {
+        return this.reviewRepository.findAll().parallelStream().filter(x -> x.getIdCostumer() == idCustomer && x.getIdAgency() == idAgency).toList();
+    }
+
+
 }
