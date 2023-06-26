b8014bb0411a7ea13df7f57c0e8121bcbfa3c3c9
MichelaDiBiase <michela.dibiase@studenti.unicam.it>
2023-06-26 18:16:14 +0200

    created Optional in MailRepository

--- src/main/java/it/unicam/cs/ids/loyaltyplatform/repository/MailRepository.java
+++ src/main/java/it/unicam/cs/ids/loyaltyplatform/repository/MailRepository.java
@@ -1,9 +1,13 @@
 package it.unicam.cs.ids.loyaltyplatform.repository;
 
 import it.unicam.cs.ids.loyaltyplatform.entity.platformservices.Mail;
+import it.unicam.cs.ids.loyaltyplatform.entity.platformservices.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MailRepository extends JpaRepository<Mail, Integer> {
+    Optional<Mail> findByMail(String mail);
 }
