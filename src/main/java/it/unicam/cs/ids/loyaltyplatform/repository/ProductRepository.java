e741806edbb1451e830bbf3c3afdc50fe3dae892
MichelaDiBiase <michela.dibiase@studenti.unicam.it>
2023-06-26 16:22:05 +0200

    created Optional in ProductRepository

--- src/main/java/it/unicam/cs/ids/loyaltyplatform/repository/ProductRepository.java
+++ src/main/java/it/unicam/cs/ids/loyaltyplatform/repository/ProductRepository.java
@@ -2,7 +2,11 @@ package it.unicam.cs.ids.loyaltyplatform.repository;
 
 import it.unicam.cs.ids.loyaltyplatform.entity.platformservices.Product;
 import it.unicam.cs.ids.loyaltyplatform.entity.platformservices.Review;
+import it.unicam.cs.ids.loyaltyplatform.entity.premiumprogram.FidelityCard;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+    Optional<Product> findByName(String name);
 }
