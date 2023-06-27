464c09a80dd9c5940be92c7ade08d896471bca92
MichelaDiBiase <michela.dibiase@studenti.unicam.it>
2023-06-27 11:27:57 +0200

    created check in getReviewByAgencyId method in ReviewServiceService

--- src/main/java/it/unicam/cs/ids/loyaltyplatform/service/ReviewService.java
+++ src/main/java/it/unicam/cs/ids/loyaltyplatform/service/ReviewService.java
@@ -39,8 +39,9 @@ public class ReviewService {
     }
 
     public List<Review> getReviewByAgencyId(Integer idAgency) {
-
-
+        if(reviewRepository.findAll().parallelStream().noneMatch(x -> x.getIdAgency().equals(idAgency))) {
+            throw new EntityNotFoundException("The id(" + idAgency + ") of the id does not exist");
+        }
         return this.reviewRepository.findAll().parallelStream().filter(x -> x.getIdAgency() == (idAgency)).toList();
     }
 
