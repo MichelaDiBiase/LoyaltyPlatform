75ac84bb3517698004b7b33bc676c0af236e816e
MichelaDiBiase <michela.dibiase@studenti.unicam.it>
2023-06-27 18:40:11 +0200

    Edit CatalogService

--- src/main/java/it/unicam/cs/ids/loyaltyplatform/service/CatalogService.java
+++ src/main/java/it/unicam/cs/ids/loyaltyplatform/service/CatalogService.java
@@ -1,4 +1,36 @@
 package it.unicam.cs.ids.loyaltyplatform.service;
 
+import it.unicam.cs.ids.loyaltyplatform.entity.platformservices.Catalog;
+import it.unicam.cs.ids.loyaltyplatform.repository.CatalogRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CatalogService {
+    private final CatalogRepository catalogRepository;
+    public CatalogService(CatalogRepository catalogRepository) {
+        this.catalogRepository = catalogRepository;
+    }
+
+    public void addCatalog(Catalog catalog) {//come posso aggiungere il prodotto
+
+        this.catalogRepository.save(catalog);
+     }
+
+    public void deleteCatalog(Catalog catalog) {// come cancellare il prodotto
+
+        this.catalogRepository.delete(catalog);
+
+    }
+
+
+    public void updateCatalog(Catalog catalog) {
+        this.catalogRepository.saveAndFlush(catalog);
+    }
+
+    public List<Catalog> getCatalogByProductId(Integer idProduct) {
+        return this.catalogRepository.findAll().parallelStream().filter(x -> x.getIdProduct() == (idProduct)).toList();
+
+    }
 }
