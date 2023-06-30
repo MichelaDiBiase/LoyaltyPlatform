1b23e1580e7e7e5de9995c76aeb6929b30409c02
MichelaDiBiase <michela.dibiase@studenti.unicam.it>
2023-06-30 16:47:19 +0200

    Edit LoyaltyPlan

--- src/main/java/it/unicam/cs/ids/loyaltyplatform/entity/loyaltyplan/LoyaltyPlan.java
+++ src/main/java/it/unicam/cs/ids/loyaltyplatform/entity/loyaltyplan/LoyaltyPlan.java
@@ -1,4 +1,54 @@
 package it.unicam.cs.ids.loyaltyplatform.entity.loyaltyplan;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.criteria.CriteriaBuilder;
+
+@Entity
+@Table(name = "loyaltyplan")
 public class LoyaltyPlan {
+    @Id
+    @Column(nullable = false,unique = true)
+    private Integer id;
+
+    @Column(nullable = false)
+    private Integer idAgency;
+
+    @Column()
+    private Integer registrationCount;
+
+    public LoyaltyPlan(Integer idAgency, Integer registrationCount){
+        this.idAgency=idAgency;
+        this.registrationCount=registrationCount;
+    }
+
+    public LoyaltyPlan() {
+
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getIdAgency() {
+        return idAgency;
+    }
+
+    public void setIdAgency(Integer idAgency) {
+        this.idAgency = idAgency;
+    }
+
+    public Integer getRegistrationCount() {
+        return registrationCount;
+    }
+
+    public void setRegistrationCount(Integer registrationCount) {
+        this.registrationCount = registrationCount;
+    }
 }
