838b0b2e374b2cb16b5d8020f8aed1c4fa644d87
MichelaDiBiase <michela.dibiase@studenti.unicam.it>
2023-07-04 13:07:28 +0200

    Created List agency and add/remove method for the list in LoyaltyPlanCoalition

--- src/main/java/it/unicam/cs/ids/loyaltyplatform/entity/loyaltyplan/LoyaltyPlanCoalition.java
+++ src/main/java/it/unicam/cs/ids/loyaltyplatform/entity/loyaltyplan/LoyaltyPlanCoalition.java
@@ -1,30 +1,46 @@
 package it.unicam.cs.ids.loyaltyplatform.entity.loyaltyplan;
 
-import it.unicam.cs.ids.loyaltyplatform.entity.users.Customer;
-import jakarta.persistence.DiscriminatorValue;
-import jakarta.persistence.Entity;
+import it.unicam.cs.ids.loyaltyplatform.entity.users.Agency;
+import jakarta.persistence.*;
+
+import java.util.List;
 
 @Entity
 @DiscriminatorValue("planCoalition")
-
 public class LoyaltyPlanCoalition extends LoyaltyPlan{
 
+    @JoinColumn(name = "idAgency")
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Agency> agenciesCoalition;
+
+    public LoyaltyPlanCoalition (Integer idAgency, Integer registrationCount, List<Agency> agenciesCoalition){
+        super(idAgency,registrationCount);
+        this.agenciesCoalition = agenciesCoalition;
+    }
 
     public LoyaltyPlanCoalition (Integer idAgency, Integer registrationCount){
         super(idAgency,registrationCount);
-
     }
 
     public LoyaltyPlanCoalition () {
 
     }
 
-    @Override
-    public boolean useAdvantage(Customer customer) {
-        if (customer.getLoyaltyPlans().parallelStream().noneMatch(x-> x instanceof LoyaltyPlanCoalition && x.getIdAgency().equals(this.getIdAgency()))){
-
-            return true;
+    public void addAgencyToCoalition(Agency agency) {
+        if(agenciesCoalition.parallelStream().anyMatch(x -> x.getId().equals(agency.getId()))) {
+            throw new EntityExistsException("The id(" + agency.getId() + ") of the Agency is already in the coalition");
         }
-        return false;
+        agenciesCoalition.add(agency);
+    }
+
+    public void removeAgencyToCoalition(Agency agency) {
+        if(agenciesCoalition.parallelStream().noneMatch(x -> x.getId().equals(agency.getId()))) {
+            throw new EntityExistsException("The id(" + agency.getId() + ") of the Agency is not in the coalition");
+        }
+        agenciesCoalition.remove(agency);
+    }
+
+    public List<Agency> getAgenciesCoalition() {
+        return agenciesCoalition;
     }
 }
