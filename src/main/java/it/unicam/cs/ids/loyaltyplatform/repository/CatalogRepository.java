4823573b4935ffa18a32816362304211995e0ac0
MichelaDiBiase <michela.dibiase@studenti.unicam.it>
2023-07-04 13:14:22 +0200

    edit CatalogRepository

--- src/main/java/it/unicam/cs/ids/loyaltyplatform/repository/CatalogRepository.java
+++ src/main/java/it/unicam/cs/ids/loyaltyplatform/repository/CatalogRepository.java
@@ -1,16 +1,13 @@
 package it.unicam.cs.ids.loyaltyplatform.repository;
 
-import it.unicam.cs.ids.loyaltyplatform.entity.platformservices.Catalog;
 import it.unicam.cs.ids.loyaltyplatform.entity.platformservices.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 
-public interface CatalogRepository extends JpaRepository<Catalog, Integer> {
-    Optional<Product> findByIdProduct(Integer idProduct);
+public interface CatalogRepository extends JpaRepository<Product, Integer> {
 
 }
