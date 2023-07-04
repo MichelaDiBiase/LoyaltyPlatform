94574c8d8c3d9c2cb0a9c79e4e48d3c4a70096c9
MichelaDiBiase <michela.dibiase@studenti.unicam.it>
2023-07-04 13:08:24 +0200

    Edit LoyaltyPlanLevels

--- src/main/java/it/unicam/cs/ids/loyaltyplatform/entity/loyaltyplan/LoyaltyPlanLevels.java
+++ src/main/java/it/unicam/cs/ids/loyaltyplatform/entity/loyaltyplan/LoyaltyPlanLevels.java
@@ -1,29 +1,79 @@
 package it.unicam.cs.ids.loyaltyplatform.entity.loyaltyplan;
 
-import it.unicam.cs.ids.loyaltyplatform.entity.users.Customer;
-import jakarta.persistence.DiscriminatorValue;
-import jakarta.persistence.Entity;
+import it.unicam.cs.ids.loyaltyplatform.entity.platformservices.Coupon;
+import jakarta.persistence.*;
+
+import java.util.List;
 
 @Entity
 @DiscriminatorValue("planLevels")
-
 public class LoyaltyPlanLevels extends LoyaltyPlan {
 
-    public LoyaltyPlanLevels (Integer idAgency, Integer registrationCount){
-        super(idAgency,registrationCount);
+    private Integer level;
+    private Double moneySpent;
+    @JoinColumn(name = "idCoupon")
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Coupon> coupons;
+
+
+    public LoyaltyPlanLevels(Integer idAgency, Integer registrationCount, Integer level, Double moneySpent) {
+        super(idAgency, registrationCount);
+        this.level = level;
+        this.moneySpent = moneySpent;
+    }
+
+    public LoyaltyPlanLevels(Integer idAgency, Integer registrationCount) {
+        super(idAgency, registrationCount);
+    }
+
+    public LoyaltyPlanLevels() {
 
     }
 
-    public LoyaltyPlanLevels () {
-
-    }
-
-    @Override
-    public boolean useAdvantage(Customer customer) {
-        if (customer.getLoyaltyPlans().parallelStream().noneMatch(x-> x instanceof LoyaltyPlanLevels && x.getIdAgency().equals(this.getIdAgency()))){
-
-            return true;
+    public void incrementLevel() {
+        int i = 1;
+        while (moneySpent >= 100 * i) {
+            level = i;
+            Coupon coupon = new Coupon(5.0, getIdAgency());
+            addCoupon(coupon);
         }
-        return false;
+    }
+
+    public void calculateMoneySpent(Double money) {
+        moneySpent += money;
+        incrementLevel();
+    }
+
+    public void addCoupon(Coupon coupon) {
+        coupons.add(coupon);
+    }
+
+    public void removeCoupon(Coupon coupon) {
+        coupons.add(coupon);
+    }
+
+
+    public Integer getLevel() {
+        return level;
+    }
+
+    public Double getMoneySpent() {
+        return moneySpent;
+    }
+
+    public List<Coupon> getCoupons() {
+        return coupons;
+    }
+
+    public void setLevel(Integer level) {
+        this.level = level;
+    }
+
+    public void setMoneySpent(Double moneySpent) {
+        this.moneySpent = moneySpent;
+    }
+
+    public void setCoupons(List<Coupon> coupons) {
+        this.coupons = coupons;
     }
 }
