b1da143da1905d1723727f7316c540f0b0360b6f
MichelaDiBiase <michela.dibiase@studenti.unicam.it>
2023-07-05 22:32:06 +0200

    edit method updateProduct and deleteProduct in ProductController, edit some method in ProductService, edit ProductRepository

--- src/main/java/it/unicam/cs/ids/loyaltyplatform/repository/ProductRepository.java
+++ src/main/java/it/unicam/cs/ids/loyaltyplatform/repository/ProductRepository.java
@@ -3,8 +3,7 @@ package it.unicam.cs.ids.loyaltyplatform.repository;
 import it.unicam.cs.ids.loyaltyplatform.entity.platformservices.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
-    Optional<Product> findByName(String name);
+
 }
