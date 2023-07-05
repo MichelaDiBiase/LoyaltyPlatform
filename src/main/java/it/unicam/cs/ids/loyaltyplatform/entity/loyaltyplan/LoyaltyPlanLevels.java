60fd61065562de548995529e7fcbbe16ab2802e8
MichelaDiBiase <michela.dibiase@studenti.unicam.it>
2023-07-05 10:56:12 +0200

    edit LoyaltyPlanLevels

--- src/main/java/it/unicam/cs/ids/loyaltyplatform/entity/loyaltyplan/LoyaltyPlanLevels.java
+++ src/main/java/it/unicam/cs/ids/loyaltyplatform/entity/loyaltyplan/LoyaltyPlanLevels.java
@@ -1,26 +1,11 @@
 package it.unicam.cs.ids.loyaltyplatform.entity.loyaltyplan;
 
-import it.unicam.cs.ids.loyaltyplatform.entity.platformservices.Coupon;
 import jakarta.persistence.*;
-
-import java.util.List;
 
 @Entity
 @DiscriminatorValue("levels")
 public class LoyaltyPlanLevels extends LoyaltyPlan {
 
-    private Integer level;
-    private Double moneySpent;
-    @JoinColumn(name = "idCoupon")
-    @OneToMany(cascade = CascadeType.ALL)
-    private List<Coupon> coupons;
-
-
-    public LoyaltyPlanLevels(Integer idAgency, Integer level, Double moneySpent) {
-        super(idAgency);
-        this.level = level;
-        this.moneySpent = moneySpent;
-    }
 
     public LoyaltyPlanLevels(Integer idAgency) {
         super(idAgency);
@@ -28,52 +13,5 @@ public class LoyaltyPlanLevels extends LoyaltyPlan {
 
     public LoyaltyPlanLevels() {
 
-    }
-
-    public void incrementLevel() {
-        int i = 1;
-        while (moneySpent >= 100 * i) {
-            level = i;
-            Coupon coupon = new Coupon(5.0, getIdAgency());
-            addCoupon(coupon);
-        }
-    }
-
-    public void calculateMoneySpent(Double money) {
-        moneySpent += money;
-        incrementLevel();
-    }
-
-    public void addCoupon(Coupon coupon) {
-        coupons.add(coupon);
-    }
-
-    public void removeCoupon(Coupon coupon) {
-        coupons.add(coupon);
-    }
-
-
-    public Integer getLevel() {
-        return level;
-    }
-
-    public Double getMoneySpent() {
-        return moneySpent;
-    }
-
-    public List<Coupon> getCoupons() {
-        return coupons;
-    }
-
-    public void setLevel(Integer level) {
-        this.level = level;
-    }
-
-    public void setMoneySpent(Double moneySpent) {
-        this.moneySpent = moneySpent;
-    }
-
-    public void setCoupons(List<Coupon> coupons) {
-        this.coupons = coupons;
     }
 }
