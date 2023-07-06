a3bd0617ca91677a24ac12d9f16d0d6967582710
MichelaDiBiase <michela.dibiase@studenti.unicam.it>
2023-07-06 12:19:51 +0200

    some correction

--- src/main/java/it/unicam/cs/ids/loyaltyplatform/entity/loyaltyplan/LoyaltyPlanPoints.java
+++ src/main/java/it/unicam/cs/ids/loyaltyplatform/entity/loyaltyplan/LoyaltyPlanPoints.java
@@ -1,10 +1,9 @@
 package it.unicam.cs.ids.loyaltyplatform.entity.loyaltyplan;
 
-import it.unicam.cs.ids.loyaltyplatform.entity.platformservices.Product;
-import it.unicam.cs.ids.loyaltyplatform.entity.users.Customer;
+
 import jakarta.persistence.*;
 
-import java.util.List;
+
 
 @Entity
 @DiscriminatorValue("points")
